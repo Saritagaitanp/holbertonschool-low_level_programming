665bc2ac15fb0a524a51c5fbddc931c36fc3e246
Saritagaitanp <you@example.com>
2022-06-13 17:09:02 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,2 @@
 #include <stdio.h>
-int
-{
-puts ("Programming is like building a multilingual puzzle");
-return (0);
-}
+int {puts ("Programming is like building a multilingual puzzle"); return (0);}
