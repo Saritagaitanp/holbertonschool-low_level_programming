4763a47ef6766f0036ee50161ae91cd136cdde54
Saritagaitanp <you@example.com>
2022-06-13 19:10:15 -0700

    Print size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,10 +11,10 @@ int inttype;
 long int longinttype;
 long long int longlonginttype;
 float floattype;
-printf("Size of char: %zu bytes\n", sizeof(charType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of long int: %zu bytes\n", sizeof(longintType));
-printf("Size of long long int: %zu byte\n", sizeof(longlongintType));
+printf("Size of char: %zu bytes\n", sizeof(chartype));
+printf("Size of int: %zu bytes\n", sizeof(inttype));
+printf("Size of long int: %zu bytes\n", sizeof(longinttype));
+printf("Size of long long int: %zu byte\n", sizeof(longlonginttype));
 printf("Size of float: %zu byte\n", sizeof(floattype)); 
 return (0);
 }
