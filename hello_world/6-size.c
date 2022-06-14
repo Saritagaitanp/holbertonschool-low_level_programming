ee7d1516cca7095c62287405171a9ffdec00b7a4
Saritagaitanp <you@example.com>
2022-06-13 19:56:17 -0700

    Funcion printf size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,10 +11,10 @@ int inttype;
 long int longinttype;
 long long int longlonginttype;
 float floattype;
-printf("Size of char: %zu byte(s)\n", sizeof(chartype));
-printf("Size of int: %zu byte(s)\n", sizeof(inttype));
-printf("Size of long int: %zu byte(s)\n", sizeof(longinttype));
-printf("Size of long long int: %zu byte(s)\n", sizeof(longlonginttype));
-printf("Size of float: %zu byte(s)\n", sizeof(floattype));
+printf("Size of a char: %zu byte(s)\n", sizeof(chartype));
+printf("Size of an int: %zu byte(s)\n", sizeof(inttype));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longinttype));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longlonginttype));
+printf("Size of a float: %zu byte(s)\n", sizeof(floattype));
 return (0);
 }
