00574d7e37e7d65a647a1e3288c9acbaea4af9e3
Saritagaitanp <you@example.com>
2022-06-14 20:13:20 -0700

    Conditional if, last number

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,20 +9,20 @@
 
 int main(void)
 {
-int n, lastdigit;
+int n, lastdigi;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-lastdigit = n % 10;
+lastdigi = n % 10;
 
-if (lastdigit > 5)
-{ printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
+if (lastdigi > 5)
+{ printf("Last digit of %d is %d and is greater than 5\n", n, lastdigi);
 }
-if (lastdigit == 0)
-{ printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+if (lastdigi == 0)
+{ printf("Last digit of %d is %d and is 0\n", n, lastdigi);
 }
-if (lastdigit < 6 && lastdigit != 0)
-{ printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+if (lastdigi < 6 && lastdigi != 0)
+{ printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigi);
 }
 return (0);
 }
