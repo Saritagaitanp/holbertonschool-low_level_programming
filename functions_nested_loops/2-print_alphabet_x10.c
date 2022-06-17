e18305ce6a881c48b648b9c391306b09f5ab92ad
Saritagaitanp <you@example.com>
2022-06-17 15:03:38 -0700

    Alphabet x 10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - funtion print alphabet
+ * Descrption: print
+ * Return: On success 1
+ * @void: print alphabetx10
+ */
+
+void print_alphabet_x10(void)
+{
+char c;
+
+for (c = 'a' ; c <= 'z' ; c++)
+for (c = 'a' ; c <= 'z' ; c++)
+{
+_putchar(c);
+_putchar('\n');
+}
+_putchar('\n');
+}
