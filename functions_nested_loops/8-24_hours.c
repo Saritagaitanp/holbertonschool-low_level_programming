59b63a7d3c0c98116b8fe46d00f2d56236da688c
Saritagaitanp <you@example.com>
2022-06-19 18:22:05 -0700

    hours minutes and seconds

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- *
  *jack_bauer - function that the valor of a hour
- *@h:
+ *
  *Return: return the valor of a hour
  */
-
 void jack_bauer(void)
 {
 int h, m;
+
 for (h = 0; h < 24; h++)
 {
 for (m = 0; m < 60; m++)
@@ -18,8 +17,8 @@ _putchar((h % 10) + '0');
 _putchar(':');
 _putchar((m / 10) + '0');
 _putchar((m % 10) + '0');
-_putchar(' ');
-_putchar('\n');
+_putchar((' '));
+_putchar(('\n'));
 }
 }
 }
