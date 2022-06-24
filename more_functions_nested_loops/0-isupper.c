79a93c4050f3089985110af52e6c9ba2d55665e3
Saritagaitanp <you@example.com>
2022-06-23 20:07:19 -0700

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - check the caracter
+ * @c: input
+ * Return: 1 if c is uppercase, 0 if not
+ */
+
+int _isupper(int c)
+
+{
+
+if ((c >= 'a') && (c <= 'z'))
+
+return (1);
+else
+
+return (0);
+}
