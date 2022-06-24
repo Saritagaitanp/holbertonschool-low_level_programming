67746f094274a12df33e6eee9e9a8a73a5112f16
Saritagaitanp <you@example.com>
2022-06-24 12:58:44 -0700

    The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: result of print numberrs
+ */
+
+void print_numbers(void)
+{
+int a;
+for (a = 48 ; a <= 57 ; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+return (0);
+}
